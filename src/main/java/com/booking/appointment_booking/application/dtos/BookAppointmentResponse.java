21299d46a340f4fe73bf510da745ed48175b2b82
3b3al <gamaleyas@gmail.com>
2025-01-24 22:54:58 +0200

    Changes to expose extenal dtos

--- src/main/java/com/booking/appointment_booking/application/dtos/BookAppointmentResponse.java
+++
@@ -1,6 +0,0 @@
-package com.booking.appointment_booking.application.dtos;
-
-import java.util.Date;
-import java.util.UUID;
-
-public record BookAppointmentResponse(UUID slotId, String patientName, Date reservedAt) {}
--- src/main/java/com/booking/availability/Availability.java
+++
@@ -1,5 +0,0 @@
-package com.booking.availability;
-
-public class Availability {
-
-}
