08f9b68b12a7ceef107e3909b2c0f787912e0ceb
3b3al <gamaleyas@gmail.com>
2025-01-31 19:55:44 +0200

    Add getAllAvailableSlots api

--- src/main/java/com/booking/appointment_booking/api/BookAppointmentController.java
+++ src/main/java/com/booking/appointment_booking/api/BookAppointmentController.java
@@ -1,5 +1,7 @@
 package com.booking.appointment_booking.api;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,11 +9,16 @@ import org.springframework.web.bind.annotation.RestController;
 import com.booking.appointment_booking.application.dtos.BookappointmentRequest;
 import com.booking.appointment_booking.application.handlers.BookappointmentHandler;
 import com.booking.appointment_booking.contacts.BookAppointmentResponse;
+import com.booking.appointment_booking.domain.contracts.IGetAvailableSlots;
+import com.booking.doctor_availability.contacts.DoctorAvailabilityResponse;
 
 import lombok.AllArgsConstructor;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @RestController
@@ -19,6 +26,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 public class BookAppointmentController {
 
     private final BookappointmentHandler bookAppointmentHandler;
+    private final IGetAvailableSlots getAvailableSlots;
 
     @PostMapping("/v1/book-appointment")
     public ResponseEntity<?> addNewBookAppointment(@RequestBody BookappointmentRequest command) {
@@ -26,5 +34,11 @@ public class BookAppointmentController {
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
+    @GetMapping("/v1/all-available-slots")
+    public ResponseEntity<?> getAllAvailableSlots() {
+        List<DoctorAvailabilityResponse> response = getAvailableSlots.getAllAvailableSlots();
+        return ResponseEntity.status(HttpStatus.OK).body(response); 
+    }
+    
     
 }
