973132146354d67988c5000a3f5145404cab8e05
3b3al <gamaleyas@gmail.com>
2025-01-31 19:49:58 +0200

    Change DoctorAvailabilityResponse package to contacts

--- src/main/java/com/booking/doctor_availability/dtos/DoctorAvailabilityResponse.java
+++
@@ -1,7 +0,0 @@
-package com.booking.doctor_availability.dtos;
-
-
-import java.util.Date;
-import java.util.UUID;
-
-public record DoctorAvailabilityResponse(UUID slotId, Date time, UUID doctorId, String doctorName, boolean isReserved, double cost) {}
