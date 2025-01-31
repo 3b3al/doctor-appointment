f971a558063573f89702500e81cf7d77fcf8c7d0
3b3al <gamaleyas@gmail.com>
2025-01-31 19:50:17 +0200

    remove PackageInfo as its not used

--- src/main/java/com/booking/appointment_booking/PackageInfo.java
+++
@@ -1,9 +0,0 @@
-package com.booking.appointment_booking;
-
-import org.springframework.modulith.ApplicationModule;
-
-@ApplicationModule(
-    allowedDependencies = {"com.booking.doctor_availability"})
-public class PackageInfo {
-
-}
