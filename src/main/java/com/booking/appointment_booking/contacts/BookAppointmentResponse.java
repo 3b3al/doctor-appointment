c1c499a1af0374b4a55b6bd823c61d9ce7d644e9
3b3al <gamaleyas@gmail.com>
2025-01-31 19:52:11 +0200

    Expose needed dtos and methods

--- src/main/java/com/booking/appointment_booking/contacts/BookAppointmentResponse.java
+++ src/main/java/com/booking/appointment_booking/contacts/BookAppointmentResponse.java
@@ -2,4 +2,9 @@ package com.booking.appointment_booking.contacts;
 import java.util.Date;
 import java.util.UUID;
 
+import org.springframework.modulith.ApplicationModule;
+
+@ApplicationModule(
+    allowedDependencies = {"com.booking.doctor_availability"},
+    type = ApplicationModule.Type.OPEN)
 public record BookAppointmentResponse(UUID slotId, String patientName, Date reservedAt) {}
--- src/main/java/com/booking/appointment_booking/domain/contracts/IGetAvailableSlots.java
+++ src/main/java/com/booking/appointment_booking/domain/contracts/IGetAvailableSlots.java
@@ -1,8 +1,16 @@
 package com.booking.appointment_booking.domain.contracts;
 
-import java.util.Date;
+import java.util.List;
 import java.util.UUID;
 
+import org.springframework.modulith.ApplicationModule;
+
+import com.booking.doctor_availability.contacts.DoctorAvailabilityResponse;
+
+@ApplicationModule(
+    allowedDependencies = {"com.booking.doctor_availability"},
+    type = ApplicationModule.Type.OPEN)
 public interface IGetAvailableSlots {
-    boolean isSlotAvailable(UUID slotId, Date reservedAt);
+    boolean isSlotAvailable(UUID slotId);
+    List<DoctorAvailabilityResponse> getAllAvailableSlots();
 }
